b4d597f34bbe95cc7c07fda1b8e18b9e771c516d
harem0king <thanhloc@example.com>
2025-06-21 01:55:23 +0700

    giao dien dnhap

---
+++ app/src/main/java/com/example/cse441_project/login_logout/inLogin.java
@@ -0,0 +1,88 @@
+package com.example.cse441_project.login_logout;
+
+import static android.os.Build.VERSION_CODES.R;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.cse441_project.FoodItem.HomeActivity;
+
+import com.example.cse441_project.Model.Employee;
+import com.example.cse441_project.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+public class inLoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore firestore;
+    private Button startbtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.intro_activity);
+
+        // Khởi tạo FirebaseAuth và FirebaseFirestore
+        firebaseAuth = FirebaseAuth.getInstance();
+        firestore = FirebaseFirestore.getInstance();
+        startbtn = findViewById(R.id.btnStart);
+        autoLogin();
+        startbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(inLoginActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    private void autoLogin() {
+
+        SharedPreferences sharedPreferences = getSharedPreferences("LoginPrefs", MODE_PRIVATE);
+        String username = sharedPreferences.getString("username", null);
+        String password = sharedPreferences.getString("password", null);
+        if (username == null || password == null) return;
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("Employee")
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            Employee employee = document.toObject(Employee.class);
+                            if (employee != null) {
+                                String usernameFromDb = employee.getUsername(); // Lấy username từ đối tượng Employee
+                                String passwordFromDb = employee.getPassword(); // Lấy password từ đối tượng Employee
+
+                                if (username.equals(usernameFromDb) && password.equals(passwordFromDb)) {
+
+                                    Toast.makeText(this, "Đăng nhập thành công!", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(this, HomeActivity.class);
+                                    startActivity(intent);
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                });
+    }
+}
+
+
