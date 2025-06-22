2cd24da382254bab800ec80cd7fa95b0a488744c
harem0king <thanhloc@example.com>
2025-06-22 10:41:29 +0700

    abc

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,10 @@ android {
 }
 
 dependencies {
+    implementation ("androidx.legacy:legacy-support-v4:1.0.0")
+    implementation ("com.google.android.gms:play-services-base:18.3.0")
+
+    implementation("com.github.bumptech.glide:glide:4.16.0")
     implementation(platform("com.google.firebase:firebase-bom:33.15.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation(libs.appcompat)
